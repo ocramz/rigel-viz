4988ff3f93fc1ae3c84309c12275afe3fdf9dce5
ocramz <zocca.marco@gmail.com>
2019-02-10 18:39:25 +0100

    hvega example works

--- src/Lucid/VegaLite.hs
+++ src/Lucid/VegaLite.hs
@@ -1,13 +1,68 @@
-{-# language OverloadedStrings #-}
+{-# language OverloadedStrings, ExtendedDefaultRules #-}
 module Lucid.VegaLite where
 
 import Graphics.Vega.VegaLite
 import Lucid
+import Lucid.PreEscaped (scriptSrc)
 
 import qualified Data.Aeson as A
 
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T (decodeUtf8)
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+
+import Data.Monoid
+
+vegaCDN, vegaLiteCDN, vegaEmbedCDN :: Monad m => HtmlT m ()
+vegaCDN = scriptSrc "https://cdn.jsdelivr.net/npm/vega@4"
+vegaLiteCDN = scriptSrc "https://cdn.jsdelivr.net/npm/vega-lite@2.0.0"
+vegaEmbedCDN = scriptSrc "https://cdn.jsdelivr.net/npm/vega-embed@3"
+
+doc :: VegaLite -> Html ()
+doc vl = doctypehtml_ $ html_ $ do
+  meta_ [charset_ "UTF-8"]
+  head_ $ do
+    vegaCDN
+    vegaLiteCDN
+    vegaEmbedCDN
+  with div_ [id_ "vis"] ""
+  body_ $ 
+    script_ $ T.decodeUtf8 $ LBS.toStrict ("const spec =" <> A.encode (fromVL vl) <> "; vegaEmbed('#vis', spec).then(result => console.log(result)).catch(console.warn);" :: LBS.ByteString) 
+
+moo = renderToFile "test.html" (doc t1)
 
 
+--  <meta charset="UTF-8"> 
+
+
+{-
+.then(result => console.log(result))
+      .catch(console.warn);
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Import Vega 4 & Vega-Lite 3 (does not have to be from CDN) -->
+  <script src="https://cdn.jsdelivr.net/npm/vega@[VERSION]"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vega-lite@[VERSION]"></script>
+  <!-- Import vega-embed -->
+  <script src="https://cdn.jsdelivr.net/npm/vega-embed@[VERSION]"></script>
+</head>
+<body>
+
+<div id="vis"></div>
+
+<script type="text/javascript">
+  var spec = "https://raw.githubusercontent.com/vega/vega/master/docs/examples/bar-chart.vg.json";
+  vegaEmbed('#vis', spec).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+</script>
+</body>
+</html>
+-}
 
 
 -- example data
