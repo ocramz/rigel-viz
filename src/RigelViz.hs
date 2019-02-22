34579dd895c438f1e128315ca3f5260d2a3102a5
ocramz <zocca.marco@gmail.com>
2019-02-22 17:47:59 +0100

    basic encoding works

--- src/RigelViz.hs
+++ src/RigelViz.hs
@@ -1,4 +1,4 @@
-{-# language OverloadedStrings, DeriveGeneric #-}
+{-# language OverloadedStrings, DeriveGeneric, LambdaCase #-}
 module RigelViz where
 
 import GHC.Generics (Generic(..))
@@ -8,29 +8,81 @@ import Lucid.PreEscaped (scriptSrc)
 import Lucid.VegaLite (mkVegaHtml)
 
 import qualified Data.Aeson as A
+import Data.Aeson ((.=))
+import qualified Data.Aeson.Encode.Pretty as A (encodePretty)
 
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T (decodeUtf8)
 
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as LBS
+-- import qualified Data.ByteString as BS hiding (pack)
+import qualified Data.ByteString.Lazy.Char8 as BS (unpack)
+-- import qualified Data.ByteString.Lazy as LBS
 
 import Data.Monoid
 
-schema :: BS.ByteString
-schema = "https://vega.github.io/schema/vega-lite/v3.json"
+data TestValue = TV { tva :: Int, tvb :: Double } deriving (Eq, Show, Generic)
+instance A.ToJSON TestValue
+testVs = [TV 1 3.2, TV 2 5.4]
 
--- data Data
--- data Mark
--- data Encoding
 
-data VLSpec = VLSpec Data Mark View
+-- | The current schema version is 3
+schema :: Int -> String
+schema vn = mconcat ["https://vega.github.io/schema/vega-lite/v", show vn,".json"]
 
-data Data = DataJSON | DataURL deriving (Eq, Show, Generic)
+-- | Specification of a vega-lite plot
+data VLSpec a = VLSpec {
+    vlsWidth :: Int
+  , vlsHeight :: Int
+  , vlsData :: Data a
+  , vlsView :: View
+  } deriving (Eq, Show, Generic)
+instance A.ToJSON a => A.ToJSON (VLSpec a) where
+  toJSON (VLSpec w h dats view) = case view of
+    VSingle mark enc -> A.object $ [
+        "mark" .= mark
+      , "encoding" .= enc
+      ] ++ defs
+    where
+      defs = [
+        "$schema" .= schema 3
+        , "width" .= w
+        , "height" .= h
+        , "data" .= dats
+        ]
 
-data Mark = MarkPoint | MarkBar deriving (Eq, Show, Generic)
 
-data View = ViewSingle | ViewLayer deriving (Eq, Show, Generic)
+data Data a =
+    DataJSON [a]
+  | DataURL String deriving (Eq, Show, Generic)
+instance A.ToJSON a => A.ToJSON (Data a) where
+  toJSON = \case
+    DataJSON vs -> A.object ["values" .= vs]
+
+data View =
+    VSingle Mark Encoding
+  | VLayer [View]
+  deriving (Eq, Show, Generic)
+instance A.ToJSON View 
+
+data Mark = MPoint | MRect | MBar deriving (Eq, Show, Generic)
+instance A.ToJSON Mark where
+  toJSON = \case
+    MPoint -> "point"
+    MRect -> "rect"
+
+data Encoding = Enc { encName :: T.Text, encMd :: EncMetadata } deriving (Eq, Show, Generic)
+instance A.ToJSON Encoding where
+  toJSON (Enc en emd) = A.object [en .= emd]
+
+data EncMetadata = EncMetadata { emField :: String, emType :: EncodingType } deriving (Eq, Show, Generic)
+instance A.ToJSON EncMetadata where
+  toJSON (EncMetadata f t) = A.object [ "field" .= f, "type" .= t]
+
+data EncodingType = ETNominal | ETQuantitative deriving (Eq, Show, Generic)
+instance A.ToJSON EncodingType where
+  toJSON = \case
+    ETNominal -> "nominal"
+    ETQuantitative -> "quantitative"
 
 {-
 
